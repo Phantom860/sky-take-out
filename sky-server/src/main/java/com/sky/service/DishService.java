6e68a01587bf11aea7dfe0a4bb9f6b65c560fe6c
Phantom <3276496565@qq.com>
2025-03-16 10:46:37 +0800

    菜品启售停售

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -9,6 +9,7 @@ import com.sky.vo.DishVO;
 import java.util.List;
 
 public interface DishService {
+
     //新增菜品和对应的口味
     public void saveWithFlavor(DishDTO dishDTO);
 
@@ -44,4 +45,12 @@ public interface DishService {
      * @return
      */
     List<Dish> list(Long categoryId);
+
+    /**
+     * 菜品启售停售
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
+
 }
